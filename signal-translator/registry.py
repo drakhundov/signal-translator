dc9c813cfb0dc2b105ddcd5e14e58afa1d9350eb
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-12-12 17:44:47 +0300

    A registry to keep track of schemes.

--- signal-translator/registry.py
+++ signal-translator/registry.py
@@ -2,12 +2,12 @@ from custom_types import TransmissionMode
 
 from d2d_encoding import NRZ, ManchesterCode, AMI
 from d2a_modulation import ASK, FSK, PSK
-from a2d_encoding import PCM
-# from a2a_modulation import
+from a2d_modulation import PCM, DeltaModulation
+from a2a_modulation import AM, PM, FM
 
 schemes = {
     TransmissionMode.D2D: [NRZ, ManchesterCode, AMI],
     TransmissionMode.D2A: [ASK, FSK, PSK],
-    TransmissionMode.A2D: [PCM],
-    TransmissionMode.A2A: [],
+    TransmissionMode.A2D: [PCM, DeltaModulation],
+    TransmissionMode.A2A: [AM, PM, FM],
 }
